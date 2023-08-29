c903eac8229d2f99265966b50f6349ae2651db8c
CamillaSantana <126738571+CamillaSantana@users.noreply.github.com>
2023-08-29 02:56:23 -0300

    Delete Chamados directory

--- Chamados/src/main/java/com/example/Chamado.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public record Chamado (String user, String codigo, String categoria, String atividade) {
-    
-    @Override
-    public String toString(){
-        return codigo + ": " + user + " - " + categoria + " - " + atividade;
-    }
-}
